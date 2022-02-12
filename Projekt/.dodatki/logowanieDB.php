a85677e16df6e1c839f214d7cc51b042033cc7ee
mr ghost <mw.duch@student.uw.edu.pl>
2022-02-12 11:44:46 +0100

    Coś zadziałało

--- Projekt/.dodatki/logowanieDB.php
+++ Projekt/.dodatki/logowanieDB.php
@@ -1,9 +1,23 @@
+<?php
+    session_start();        
+?>
 <html>
     <?php
-        session_start();        
+
 
         $_SESSION['LOGINDB'] = $_POST['LOGINDB'];
         $_SESSION['PASSWORDDB'] = $_POST['PASSWORDDB'];
+        //echo $_SESSION['LOGINDB'];
+        //echo $_SESSION['PASSWORDDB'];
+        //echo $_SESSION['PASSWORDDB'];
+        
+        $conn = oci_pconnect($_SESSION['LOGIN'], $_SESSION['PASSWORD'], "//labora.mimuw.edu.pl/LABS");
+
+        if (!$conn) {
+            echo "Nie udało się połączyć z bazą danych, spróbuj ponownie\n";
+            $e = oci_error();
+            echo $e['message'];
+        }
 
         //$login = oci_parse($conn,"SELECT login from USERS where login=".$_SESSION['LOGINDB']."AND password=".$_SESSION['PASSWORDDB']);
         $stm = oci_parse($conn,"SELECT * from USERS");
